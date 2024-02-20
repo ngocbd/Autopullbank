c7a67615cfaca5e447319d0c9c22c5ed15b7d7a4
Ducmaster <nhayhoc@gmail.com>
2024-02-21 02:37:07 +0700

    fix(acb): headless true

--- src/gateways/gateway-factory/acbbank.services.ts
+++ src/gateways/gateway-factory/acbbank.services.ts
@@ -41,7 +41,7 @@ export class ACBBankService extends Gate {
   }
 
   async login() {
-    this.browser = await playwright.chromium.launch({ headless: false });
+    this.browser = await playwright.chromium.launch({ headless: true });
     this.context = await this.browser.newContext({
       userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
